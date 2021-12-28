6a7b27d34eae394d87249ea42418eab42694b88d
Andres Roca P <afrokap@gmail.com>
2021-12-27 23:29:00 -0500

    loading sections from 1 to 9

---
+++ 9-Further CSS props/shit.java
@@ -0,0 +1,6 @@
+List _myList_ = new ArrayList();
+public static float promedio(int... grades){
+    int gradeCount = grades.length;
+    for (int grade : grades){
+
+System.out.println("promedio : "+promedio(5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2,3,4));
